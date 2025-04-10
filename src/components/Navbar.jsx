337ca00d3a2f7e128aebda75a46205c0a541b164
Ayman Rahman <ayman2002rahman@gmail.com>
2025-04-09 21:14:38 -0500

    Started some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import logo from "../assets/yoruult.png";
-import {FaLinkedin} from "react-icons/fa";
-import {FaGithub} from "react-icons/fa";
-import {FaSquareXTwitter} from "react-icons/fa6";
-import {FaInstagram} from "react-icons/fa";
-
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLeetcode } from 'react-icons/fa';
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
     return (
